46b7df6c18e193a6898aead1081b02809f211411
Matt Young <mfsy@yahoo.com>
2023-02-22 22:10:25 +0100

    Mac packaging etc 27

--- src/utils/EnumStringMapping.cpp
+++ src/utils/EnumStringMapping.cpp
@@ -1,5 +1,5 @@
 /*======================================================================================================================
- * utils/EnumStringMapping.cpp is part of Brewken, and is copyright the following authors 2021-2022:
+ * utils/EnumStringMapping.cpp is part of Brewken, and is copyright the following authors 2021-2023:
  *   • Matt Young <mfsy@yahoo.com>
  *
  * Brewken is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License
@@ -23,10 +23,23 @@ EnumAndItsString::EnumAndItsString(QString string, int native) :
    return;
 }
 
-std::optional<int> EnumStringMapping::stringToEnumAsInt(QString const & stringValue) const {
+std::optional<int> EnumStringMapping::stringToEnumAsInt(QString const & stringValue,
+                                                        bool const caseInensitiveFallback) const {
    auto match = std::find_if(this->begin(),
                              this->end(),
                              [stringValue](EnumAndItsString const & ii){return stringValue == ii.string;});
+   //
+   // If we didn't find an exact match, we'll try a case-insensitive one if so-configured.  (We don't do this by
+   // default as the assumption is that it's rare we'll need the case insensitivity.
+   //
+   if (match == this->end() && caseInensitiveFallback) {
+      match = std::find_if(
+         this->begin(),
+         this->end(),
+         [stringValue](EnumAndItsString const & ii){return stringValue.toLower() == ii.string.toLower();}
+      );
+   }
+
    if (match == this->end()) {
       return std::nullopt;
    }
