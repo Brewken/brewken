f5f018861ce28c9b2a833c5faf99366b612e6ba5
Matt Young <mfsy@yahoo.com>
2026-05-15 10:00:26 +0200

    Merge https://github.com/Brewtarget/brewtarget/pull/1111

--- scripts/btUtils.py
+++ scripts/btUtils.py
@@ -1,5 +1,5 @@
 #-----------------------------------------------------------------------------------------------------------------------
-# scripts/btUtils.py is part of Brewken, and is copyright the following authors 2022-2025:
+# scripts/btUtils.py is part of Brewken, and is copyright the following authors 2022-2026:
 #   • Matt Young <mfsy@yahoo.com>
 #
 # Brewken is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License
@@ -140,9 +140,15 @@ def getLinuxDistroInfo():
       #
       # Now split release into major and minor
       #
-      parsedRelease = packaging.version.parse(distroInfo["release"])
-      distroInfo["major"] = parsedRelease.major
-      distroInfo["minor"] = parsedRelease.minor
+      try:
+         parsedRelease = packaging.version.parse(distroInfo["release"])
+         distroInfo["major"] = parsedRelease.major
+         distroInfo["minor"] = parsedRelease.minor
+      except Exception:
+         # On some distro (eg Debian sid), lsb_release -a returns 'Release: n/a', which makes parse() crash. Since major and
+         # minor versions are mostly used for logging, let's use dummy values if this happens
+         distroInfo["major"] = 1
+         distroInfo["minor"] = 0
 
    return distroInfo
 
